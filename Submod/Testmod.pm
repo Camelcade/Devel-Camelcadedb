bf1b2b10726b6e0ced05ab7bb10b155e994a5e5c
Alexandr Evstigneev <Alexandr.Evstigneev@jetbrains.com>
2023-02-26 17:48:29 +0400

    Removed unnecessary files

--- Submod/Testmod.pm
+++
@@ -1,7 +0,0 @@
-package Testmod;
-use strict;
-use warnings FATAL => 'all';
-
-#use Submod::Someother::Submod;
-
-1;
--- www/handler.pl
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/perl
-use strict;
-use warnings FATAL => 'all';
-use CGI;
-
-print CGI::header();
-
-print "Hello world!";
